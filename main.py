92978c3dd1b5393e3af22eb586855d3c2ab0df1d
fmars <maxime.chevillardsnc@gmail.com>
2025-10-22 18:48:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -408,6 +408,14 @@ def main():
         initial_sidebar_state="expanded"
     )
 
+    # Include the "#": #MainMenu {visibility: hidden;}
+    hide_streamlit_style = """
+            <style>
+            
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
     st.markdown("""
         <style>
